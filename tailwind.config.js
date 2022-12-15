1b48a04c2f0348ed60c71e0a6d9f1ce6fcc5a894
severment <s.hartnett12@gmail.com>
2022-12-15 18:56:15 +0000

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  purge: ['./pages/**/*{js,jsx}', './components/**/*.{js,jsx}'],
+  content: ['./pages/**/*.{html,js}', './components/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
